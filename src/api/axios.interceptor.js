ceb19e5a0efab1db8a8724d1408e0a51de4b94d7
Jesús Daniel <jesusdanielsanchezalarcon79@gmail.com>
2025-02-07 08:53:11 -0500

    axios change

--- src/api/axios.interceptor.js
+++ src/api/axios.interceptor.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 
-// export const api = axios.create({
-//   baseURL: "https://web-production-ec79f.up.railway.app/api/",
-// });
-
 export const api = axios.create({
-  baseURL: "http://192.168.150.118:8000/api/",
+  baseURL: "https://web-production-ec79f.up.railway.app/api/",
 });
+
+// export const api = axios.create({
+//   baseURL: "http://192.168.150.118:8000/api/",
+// });
